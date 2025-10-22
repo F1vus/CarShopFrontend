136fd13336747eb24565246f4348a20bb26861d7
Mambo22 <114337458+Kostya20079@users.noreply.github.com>
2025-10-22 17:49:09 +0200

    Revert "nav +"

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,34 +1,34 @@
 import { useState, useEffect } from "react";
 import api from "./api";
-import Footer from "./components/layouts/Footer"; 
 
 function App() {
   const [cars, setCars] = useState([]);
 
   useEffect(() => {
-    api.cars.getAllCars().then(response => setCars(response));
-  }, []); 
+    api.cars.getAllCars().then(response => setCars(response))
+  })
 
   return (
     <div className="container">
       {
         cars.map(car => (
-          <div className="card" key={car.id}>
-            <h2>{car.name}</h2>
-            <p>{car.description}</p>
-            <p>Price: {car.price}</p>
-            <p>Color: {car.color}</p>
-            <div className="photos">
+          <>
+            <div className="card" key={car.id}>
+              <h2>{car.name}</h2>
+              <p>{car.description}</p>
+              <p>Price: {car.price}</p>
+              <p>Color: {car.color}</p>
               {
-                car.photos.map((photo, idx) => (
-                  <img key={idx} src={photo} alt={car.name} width={"150px"}/>
+                car.photos.map(photo => (
+                  <>
+                    <img src={photo} alt={car.name} width={"150px"}/>
+                  </>
                 ))
               }
             </div>
-          </div>
+          </>
         ))
       }
-      <Footer /> {/* dodany footer */}
     </div>
   );
 }
