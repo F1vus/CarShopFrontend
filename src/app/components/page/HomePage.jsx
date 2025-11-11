909e41f43a38f10cb61c1b511277f87c309a9be4
Kostiantyn Kolesnyk <kostiakolesnik2513@gmail.com>
2025-11-11 20:45:23 +0100

    add benefits section

--- src/app/components/page/HomePage.jsx
+++ src/app/components/page/HomePage.jsx
@@ -3,6 +3,7 @@ import "styles/_home-page.scss";
 import Slider from "../UI/Slider";
 import logos from "../../assets/logos/logos";
 import Divider from "../UI/Divider";
+import womanImage from "assets/img/people/women.png";
 
 function HomePage() {
   return (
@@ -30,9 +31,24 @@ function HomePage() {
           ))}
         </Slider>
       </section>
-      <Divider right={true} />
+      <Divider />
       <section className="out-benefits">
-
+        <div className="out-benefits__info-block">
+          <h3 className="out-benefits__title">
+            Kupuj bez stresu – wybierz pewność i jakość
+          </h3>
+          <p className="out-benefits__info">
+            Działamy na rynku od 10 lat. Każdy samochód sprawdzany jest przez
+            doświadczonych mechaników, a klient otrzymuje pełną historię
+            serwisową oraz gwarancję satysfakcji.
+          </p>
+          <Link to="/cars" className="out-benefits__cta-button">
+            Wyszukaj Samochód
+          </Link>
+        </div>
+        <div className="out-benefits__image-block">
+          <img src={womanImage} alt="happy-person" />
+        </div>
       </section>
     </div>
   );
