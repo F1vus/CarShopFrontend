3d8dbeb8c43bb8fa2f491305db1021f67d02cb1c
Kostiantyn Kolesnyk <kostiakolesnik2513@gmail.com>
2025-11-11 16:24:55 +0100

    add header banner section and add styles

--- src/app/components/page/HomePage.jsx
+++ src/app/components/page/HomePage.jsx
@@ -1,7 +1,24 @@
 import { Link } from "react-router-dom";
+import "styles/_home-page.scss";
 
 function HomePage() {
-  return <Link to="/cars">Cars</Link>;
+  return (
+    <section className="home-page">
+      <header className="home-page__header wide-container">
+        <div className="home-page__content">
+          <h1 className="home-page__title">
+            Twoje nowe auto czeka – sprawdź naszą ofertę już dziś!
+          </h1>
+          <p className="home-page__subtitle">
+            Z nami oszczędzasz czas, pieniądze i stres.
+          </p>
+          <Link to="/cars" className="home-page__cta-button">
+            Wyceń swoje auto
+          </Link>
+        </div>
+      </header>
+    </section>
+  );
 }
 
 export default HomePage;
