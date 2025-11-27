81add5bc20e89b88a4a729f2dcb91cd62882c293
Kostiantyn Kolesnyk <kostiakolesnik2513@gmail.com>
2025-11-27 23:44:26 +0100

    Refactor CarCard component for profile page integration and update styles for better layout

--- src/app/components/UI/CarCard.jsx
+++ src/app/components/UI/CarCard.jsx
@@ -6,9 +6,10 @@ import { useState } from "react";
 import "styles/_car-card.scss";
 import { Link, useLocation } from "react-router-dom";
 
-function CarCard({ carInfo }) {
+function CarCard({ carInfo, isProfileCard = false, handles = {} }) {
   const [isLiked, setIsLiked] = useState(false);
-  const currentPath = useLocation().pathname
+  const currentPath = useLocation().pathname;
+  const { handleDelete, handleEdit } = handles;
 
   const {
     id,
@@ -23,57 +24,91 @@ function CarCard({ carInfo }) {
   } = carInfo;
 
   return (
-    <div className="car-card">
-      <div className="car-card__image">
-        <img src={photos[0].url} alt={name} />
-      </div>
-
-      <div className="car-card__info">
-        <div className="car-card__info-block">
-          <h3 className="car-card__info-title">
-            <Link to={currentPath + `/${id}`}>
-              {name}
-            </Link>
-          </h3>
-          <div className="car-card__info-description">{description}</div>
+    <div className={`car-card ${isProfileCard ? "car-card-profile" : ""}`}>
+      <div className="car-card__container">
+        <div className="car-card__image">
+          <img src={photos[0].url} alt={name} />
         </div>
-        <div className="car-card__footer">
-          <ul className="car-card__details">
-            <li>
-              <img src={wheel} alt="mileage" />
-              <strong>Przebieg:</strong> {mileage} km
-            </li>
-            <li>
-              <img src={calendar} alt="year" />
-              <strong>Rok:</strong> {year}
-            </li>
-            <li>
-              {petrolType.name === "Electric" ? (
-                <>
-                  <img src={battery} alt="battery" />
-                  {petrolType.name}
-                </>
+
+        <div className="car-card__info">
+          <div className="car-card__info-block">
+            <h3 className="car-card__info-title">
+              {isProfileCard ? (
+                <Link to={currentPath + `/${id}`}>{name}</Link>
               ) : (
-                <>
-                  <img src={canister} alt="canister" />
-                  <strong>Paliwo:</strong> {petrolType.name}
-                </>
+                { name }
               )}
-            </li>
-          </ul>
-          <p>Producent: {producent.name}</p>
+            </h3>
+            <div className="car-card__info-description">{description}</div>
+          </div>
+          <div className="car-card__footer">
+            <ul className="car-card__details">
+              <li>
+                <img src={wheel} alt="mileage" />
+                <strong>Przebieg:</strong> {mileage} km
+              </li>
+              <li>
+                <img src={calendar} alt="year" />
+                <strong>Rok:</strong> {year}
+              </li>
+              <li>
+                {petrolType.name === "Electric" ? (
+                  <>
+                    <img src={battery} alt="battery" />
+                    {petrolType.name}
+                  </>
+                ) : (
+                  <>
+                    <img src={canister} alt="canister" />
+                    <strong>Paliwo:</strong> {petrolType.name}
+                  </>
+                )}
+              </li>
+            </ul>
+            <p className="car-card__producer">Producent: {producent.name}</p>
+          </div>
         </div>
-      </div>
-      <div className="car-card__price">
-        {price} <span>PLN</span>
+        <div className="car-card__price">
+          {price} <span>PLN</span>
+        </div>
+        {!isProfileCard && (
+          <div
+            className="car-card__fav"
+            onClick={() => setIsLiked((prev) => !prev)}
+          >
+            <i className={`bi bi-heart${!isLiked ? "" : "-fill"}`}></i>
+          </div>
+        )}
       </div>
 
-      <div
-        className="car-card__fav"
-        onClick={() => setIsLiked((prev) => !prev)}
-      >
-        <i className={`bi bi-heart${!isLiked ? "" : "-fill"}`}></i>
-      </div>
+      {isProfileCard && (
+        <>
+          <hr className="card-divider" />
+          <div className="control-block">
+            <div className="id-section">
+              <strong>Id: {id}</strong>
+            </div>
+            <div className="profile-ads__card-actions">
+              <button
+                type="button"
+                className="profile-ads__btn profile-ads__btn--edit"
+                onClick={() => handleEdit(carInfo)}
+                aria-label={`Edytuj ogłoszenie ${id}`}
+              >
+                Edytuj
+              </button>
+
+              <button
+                type="button"
+                className="profile-ads__btn profile-ads__btn--delete"
+                onClick={() => handleDelete(id)}
+              >
+                Usuń
+              </button>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
