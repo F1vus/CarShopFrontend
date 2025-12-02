4124303f5fd45545e23cf25f045c601a040c28b5
Kostiantyn Kolesnyk <kostiakolesnik2513@gmail.com>
2025-12-02 17:02:58 +0100

    adaptive styles

--- src/app/components/page/ProfilePage/ProfileMessages.jsx
+++ src/app/components/page/ProfilePage/ProfileMessages.jsx
@@ -1,40 +1,44 @@
 import chatting from "assets/img/auth-page/chatting.svg";
-import chatting_pgone from "assets/img/auth-page/chatting-phone.svg"
-import "styles/profilePage/_profile-messages.scss"
-import {Link} from "react-router-dom";
+import chattingPhone from "assets/img/auth-page/chatting-phone.svg";
+import "styles/profilePage/_profile-messages.scss";
+import { Link } from "react-router-dom";
 
 function ProfileMessages() {
-
   return (
-      // <h1>Wiadomości</h1>
-      <>
-          <section className="profile-messages">
+    <section className="profile-messages">
+      <h1 className="profile-messages__title">Wiadomości</h1>
 
-              <h1 className="profile-messages__news">Wiadomości</h1>
+      <div className="profile-messages__content">
+        <aside className="profile-messages__sidebar">
+          <img
+            className="profile-messages__image"
+            src={chatting}
+            alt="chatting"
+          />
+          <h4 className="profile-messages__subtitle">
+            Wybór konwersacji do przejrzenia i udzielenia odpowiedzi
+          </h4>
+          <p className="profile-messages__text">
+            W tym miejscu pojawią się rozmowy rozpoczęte przez kupujących
+          </p>
+          <p className="profile-messages__text">
+            Jeśli chcesz coś sprzedać lub oferujesz usługę, zacznij od{" "}
+            <Link to="/sellcar" className="profile-messages__link">
+              dodania ogłoszenia
+            </Link>
+          </p>
+        </aside>
 
-            <div className="profile-messages__content">
-                <aside className="profile-messages__chose">
-                    <img className="profile-messages__image" src={chatting} alt="chatting" />
-                    <h4 className="profile-messages__subtitle">Wybór konwersacji do przejrzenia i udzielenia odpowiedzi</h4>
-                    <p className="profile-messages__text">W tym miejscu pojawią się rozmowy rozpoczęte przez kupujących</p>
-                    <p className="profile-messages__text">Jeśli chcesz coś sprzedać lub oferujesz usługę, zacznij od <Link to="/sellcar" className="profile-messages__link"> dodania ogłoszenia</Link> </p>
-                </aside>
-
-
-
-                <article className="profile-messages__article">
-                    <div>
-                        <img src={chatting_pgone} alt="chatting_pgone" />
-                        <h4 className="profile-messages__subtitle">Wybór konwersacji do przejrzenia i udzielenia odpowiedzi</h4>
-                    </div>
-                </article>
-            </div>
-
-
-          </section>
-
-
-      </>
+        <article className="profile-messages__main">
+          <div className="profile-messages__main-inner">
+            <img src={chattingPhone} alt="chatting phone" />
+            <h4 className="profile-messages__subtitle">
+              Wybór konwersacji do przejrzenia i udzielenia odpowiedzi
+            </h4>
+          </div>
+        </article>
+      </div>
+    </section>
   );
 }
 
